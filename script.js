38e2cce78fec781bc1040645101222bfe366bf28
Sirajsahin <96188626+Sirajsahin@users.noreply.github.com>
2022-05-31 16:02:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// var swiper = new Swiper(".review-slider", {
+//     spaceBetween: 2,
+//     loop:true,
+//     autoplay: {
+//         delay: 3000,
+//         disableOnInteraction: false,
+//     },
+//     breakpoints: {
+//         640: {
+//           slidesPerView: 1,
+//         },
+//         768: {
+//           slidesPerView: 1,
+//         },
+//         1024: {
+//           slidesPerView: 1,
+//         },
+//     },
+// });
